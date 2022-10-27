50bb3226b688db070a3e48bb5bcf15777b56b230
Maarten Peels <maartenpeels@gmail.com>
2022-10-27 16:57:50 +0200

    Refactor

---
+++ Engine/Layer.h
@@ -0,0 +1,22 @@
+//
+// Created by Maarten Peels on 27/10/2022.
+//
+
+#ifndef LGE_LAYER_H
+#define LGE_LAYER_H
+
+namespace lve {
+    class Layer
+    {
+    public:
+        virtual ~Layer() = default;
+
+        virtual void OnAttach() {}
+        virtual void OnDetach() {}
+
+        virtual void OnUpdate(float ts) {}
+        virtual void OnUIRender() {}
+    };
+}
+
+#endif //LGE_LAYER_H
